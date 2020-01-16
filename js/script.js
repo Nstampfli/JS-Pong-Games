bcddcdba87f91c14f690319362c598e50e42e7a3
Nstampfli <nathan.stampfli@hetic.net>
2020-01-16 22:44:07 +0100

    move player top bottom

--- js/script.js
+++ js/script.js
@@ -13,12 +13,57 @@ player.src = "../assets/player.png";
 computer.src = "../assets/computer.png";
 ball.src = "../assets/ball.png";
 
-function draw() {
-  context.drawImage(board, 0, 0);
-  context.drawImage(player, 0, 200);
-  context.drawImage(computer, canvas.width - computer.width, 200);
-  context.drawImage(ball, canvas.width / 2 - ball.width / 2, canvas.height / 2);
-  requestAnimationFrame(draw);
+//Variable
+let playerX = 0;
+let playerY = canvas.height / 2 - 50;
+let computerX = canvas.width - 10;
+let computerY = canvas.height / 2 - 50;
+let ballX = canvas.width / 2 - 5;
+let ballY = canvas.height / 2;
+let speedballX = 2;
+let speedballY = 2;
+
+//Function Player Move
+function playerMove() {
+  switch (event.keyCode) {
+    case 38:
+      if (playerY > 0) {
+        playerMoveTop();
+      }
+      break;
+    case 40:
+      if (playerY < canvas.height - player.height) {
+        playerMoveBottom();
+      }
+      break;
+    default:
+      break;
+  }
+}
+// Function Player Moove Top
+function playerMoveTop() {
+  playerY -= 10;
+}
+// Function Player Moove Left
+function playerMoveBottom() {
+  playerY += 10;
 }
 
-draw();
+//Function Draw to image placement and drawing
+function draw() {
+  context.drawImage(board, 0, 0);
+  context.drawImage(player, playerX, playerY);
+  context.drawImage(computer, computerX, computerY);
+  context.drawImage(ball, ballX, ballY);
+}
+
+//Function Play
+function play() {
+  window.addEventListener("keydown", playerMove);
+  computerMove();
+  ballMove();
+  draw();
+  requestAnimationFrame(play);
+}
+//Call Function
+play();
